050c6b63d6ff12549bae8f5e9c6bb936edeaac90
LAPTOP-B6RFP6JC\sandeep <sandeepkumar.cse2@gmail.com>
2018-05-01 20:29:48 +0530

    1st version

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="df18addf-a776-4c17-b450-0ab90dab7ac7")
+public class DataBindingInfo {}
